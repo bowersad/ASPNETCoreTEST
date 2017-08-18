f7e77f855f6b879c7d6de77dcfa7abd06b105617
bowersad <abowers@geo-blue.com>
2017-08-18 09:14:14 -0400

    Forgot the Solution

---
+++ PhilameterAPI/PhilameterAPI/Controllers/RootController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace PhilameterAPI.Controllers
+{
+    public class RootController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
